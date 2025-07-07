093c2588976c76bf1a70e7f8e00de16944788267
trevorwrightdev <trevor.wright.710@gmail.com>
2025-07-07 09:15:14 -0700

    set up maplibre-gl

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,6 @@
-
+var map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://demotiles.maplibre.org/style.json', // style URL
+    center: [0, 0], // starting position [lng, lat]
+    zoom: 1, // starting zoom
+})
